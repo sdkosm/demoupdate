fee0ba629585ef9fc24fd6a3039936cc34c56b42
Deepak Lodha <deepak.lodha@nexturn.com>
2022-12-02 16:26:09 +0530

    update project

---
+++ fullstack-backend/src/main/java/com/codewitharjun/fullstackbackend/controller/UserController.java
@@ -0,0 +1,58 @@
+package com.codewitharjun.fullstackbackend.controller;
+
+import com.codewitharjun.fullstackbackend.exception.ResourceNotFoundException;
+import com.codewitharjun.fullstackbackend.exception.UserNotFoundException;
+import com.codewitharjun.fullstackbackend.model.User;
+import com.codewitharjun.fullstackbackend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/* Created by Arjun Gautam */
+@RestController
+@CrossOrigin("http://localhost:3000")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/user")
+    User newUser(@RequestBody User newUser) {
+        return userRepository.save(newUser);
+    }
+
+    @GetMapping("/users")
+    List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @GetMapping("/user/{id}")
+    User getUserById(@PathVariable Long id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("not found"));
+    }
+
+    @PutMapping("/user/{id}")
+    User updateUser(@RequestBody User newUser, @PathVariable Long id) {
+        return userRepository.findById(id)
+                .map(user -> {
+                    user.setUsername(newUser.getUsername());
+                    user.setName(newUser.getName());
+                    user.setEmail(newUser.getEmail());
+                    return userRepository.save(user);
+                }).orElseThrow(() -> new ResourceNotFoundException("not found"));
+    }
+
+    @DeleteMapping("/user/{id}")
+    String deleteUser(@PathVariable Long id){
+        if(!userRepository.existsById(id)){
+            throw new UserNotFoundException(id);
+        }
+        userRepository.deleteById(id);
+        return  "User with id "+id+" has been deleted success.";
+    }
+
+
+
+}
